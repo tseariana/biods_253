a2b474ad6c796d76a192cd6ff9bf5c063cfee595
Kristy Carpenter <kcarp@stanford.edu>
2023-01-17 14:53:52 -0800

    generating graph

--- pascals_tri.py
+++ pascals_tri.py
@@ -1,10 +1,29 @@
 import numpy as np
 import math
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+import time
 
-n= 15
-for row in range(n):
-    line= np.array([])
-    for num in range(row+1):
-        x=  math.factorial(row)/(math.factorial(num)* math.factorial(row-num))
-        line= np.append(line, x)
-    print(line)
+
+def ariana_pascal(n):
+    for row in range(n):
+        line= np.array([])
+        for num in range(row+1):
+            x=  math.factorial(row)/(math.factorial(num)* math.factorial(row-num))
+            line= np.append(line, x)
+        print(line)
+
+
+times = []
+n_iters = 200
+for i in range(n_iters):
+    start = time.time()
+    ariana_pascal(i+1)
+    end = time.time()
+    times.append(end - start)
+
+plt.scatter(x=range(n_iters), y=times)
+plt.xlabel("number of lines")
+plt.ylabel("time to print")
+plt.savefig("pascal_time.png")
